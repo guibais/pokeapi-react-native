1a61e11f4c4db54195b77537faeeba2f724a20f8
Guilherme Bais <baissousa.guilherme@gmail.com>
2022-12-29 17:35:58 -0300

    fix: added generic error translation message

--- utils/ReactQueryHelper.ts
+++ utils/ReactQueryHelper.ts
@@ -8,10 +8,11 @@ import {
   UseQueryOptions,
 } from "react-query";
 import Toast from "react-native-toast-message";
+import { useTranslation } from "react-i18next";
 
 export const errorHandler = async (error: unknown) => {
-  const genericErrorMessage =
-    "Ops! We are having some problems. Please try again later.";
+  const { t } = useTranslation();
+  const genericErrorMessage = t("error.generic");
   const hasGenericToastError = !!Array.prototype.slice
     .call(document.getElementsByTagName("span"))
     .filter((el) => el.textContent.trim() === genericErrorMessage.trim())
